b147f3e9f07e3953ae9ec879191dd7bad4e9fbb4
Bamberek <kabaczek99@gmail.com>
2024-08-05 22:24:56 +0200

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,9 +16,29 @@ const config = {
       padding: "2rem",
       screens: {
         "2xl": "1400px",
+        xs: "360px",
       },
     },
     extend: {
+      colors: {
+        blue: {
+          100: "#B4C6EE",
+          400: "#417BFF",
+          500: "#3371FF",
+        },
+        red: {
+          400: "#DD4F56",
+          500: "#DC4349",
+        },
+        dark: {
+          100: "#09111F",
+          200: "#0B1527",
+          300: "#0F1C34",
+          350: "#12213B",
+          400: "#27344D",
+          500: "#2E3D5B",
+        },
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -28,6 +48,10 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+      },
+      backgroundImage: {
+        doc: "url(/assets/images/doc.png)",
+        modal: "url(/assets/images/modal.png)",
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
