4329cfb6d08dc20335b06ce0225892e1e84cec29
Bamberek <kabaczek99@gmail.com>
2024-08-07 04:45:59 +0200

    updated home page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,46 @@
+import AddDocumentButton from "@/components/AddDocumentButton";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) {
+    redirect("/sign-in");
+  }
+
+  const documents = [];
+
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+          <AddDocumentButton
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default Home;
