e0f1fd6de8451b6beca44b47d07ebff250e39038
Bamberek <kabaczek99@gmail.com>
2024-08-07 04:50:34 +0200

    created add document button component

---
+++ components/AddDocumentButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { createRoom } from "@/lib/actions/room.actions";
+import { useRouter } from "next/navigation";
+
+const AddDocumentButton = ({ userId, email }: AddDocumentBtnProps) => {
+  const router = useRouter();
+
+  const handleAddDocument = async () => {
+    try {
+      const room = await createRoom({ userId, email });
+
+      if (room) {
+        router.push(`/documents/${room.id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Button
+      type="submit"
+      onClick={handleAddDocument}
+      className="gradient-blue flex gap-1 shadow-md"
+    >
+      <Image
+        src="/assets/icons/add.svg"
+        alt="add-icon"
+        width={24}
+        height={24}
+      />
+      <p className="hidden sm:block">Start a new document</p>
+    </Button>
+  );
+};
+
+export default AddDocumentButton;
