05aaaa6b07169e13f6075778b48e20ba09ba4fc5
Bamberek <kabaczek99@gmail.com>
2024-08-07 04:49:39 +0200

    created room actions

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
+
+export const createRoom = async ({ userId, email }: CreateDocumentParams) => {
+  const roomId = nanoid();
+
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: "Untitled",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ["room:write"],
+    });
+
+    revalidatePath("/");
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`error while creating a room:${error}`);
+  }
+};
+
+export const getRoom = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    if (!hasAccess) {
+      throw new Error("You don't have access to this document");
+    }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error fetching room: ${error}`);
+  }
+};
