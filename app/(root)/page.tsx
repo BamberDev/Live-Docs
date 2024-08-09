f7b87c4c4874d791acb387a3d8656d371007bb28
Bamberek <kabaczek99@gmail.com>
2024-08-09 05:15:19 +0200

    updated home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import AddDocumentButton from "@/components/AddDocumentButton";
+import { DeleteModal } from "@/components/DeleteModal";
 import Header from "@/components/Header";
 import { getRooms } from "@/lib/actions/room.actions";
 import { dateConverter } from "@/lib/utils";
@@ -58,6 +59,7 @@ const Home = async () => {
                     </p>
                   </div>
                 </Link>
+                <DeleteModal roomId={id} />
               </li>
             ))}
           </ul>
