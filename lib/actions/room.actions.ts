52b29b9a853ddcc93136d15c62b1d9bf204f92c3
Bamberek <kabaczek99@gmail.com>
2024-08-09 05:14:50 +0200

    created delete document action

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -4,6 +4,7 @@ import { nanoid } from "nanoid";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
 import { getAccessType, parseStringify } from "../utils";
+import { redirect } from "next/navigation";
 
 export const createRoom = async ({ userId, email }: CreateDocumentParams) => {
   const roomId = nanoid();
@@ -129,5 +130,15 @@ export const removeCollaborator = async ({
     return parseStringify(updatedRoom);
   } catch (error) {
     console.log(`Error removing collaborator: ${error}`);
+  }
+};
+
+export const deleteDocument = async (roomId: string) => {
+  try {
+    await liveblocks.deleteRoom(roomId);
+    revalidatePath("/");
+    redirect("/");
+  } catch (error) {
+    console.log(`Error happened while deleting a room: ${error}`);
   }
 };
